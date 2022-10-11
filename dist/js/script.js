4e4eefbd7661f32a0682fc87a163a01bede519d4
Romadoni <romadoniofficial@gmail.com>
2022-10-11 15:47:26 +0700

    update porto

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,58 +1,84 @@
 // Hamburger
 
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function() {
-  hamburger.classList.toggle('hamburger-active');
-  navMenu.classList.toggle('hidden');
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
 // when click not in hamburger menu
 
-window.addEventListener('click', function(e){
-
-  if(e.target != hamburger && e.target != navMenu ){
-    hamburger.classList.remove('hamburger-active');
-    navMenu.classList.add('hidden');
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
   }
 });
 
 // Navbar Fixed
-window.onscroll = function() {
-  const header = document.querySelector('header');
+window.onscroll = function () {
+  const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
-  const toTop = document.querySelector('#to-top');
+  const toTop = document.querySelector("#to-top");
 
-  if(window.pageYOffset > fixedNav){
-    header.classList.add('navbar-fixed');
-    toTop.classList.remove('hidden');
-    toTop.classList.add('flex');
-  }else{
-    header.classList.remove('navbar-fixed');
-    toTop.classList.remove('flex');
-    toTop.classList.add('hidden');
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
 
 // dark Toggle mode
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
 
-darkToggle.addEventListener('click', function(){
-
-  if(darkToggle.checked){
-    html.classList.add('dark');
-    localStorage.theme = 'dark';
-  }else{
-    html.classList.remove('dark');
-    localStorage.theme = 'light'
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
 
 // localstorage dark & light mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
   darkToggle.checked = true;
 } else {
-    darkToggle.checked = false;
+  darkToggle.checked = false;
 }
+
+// contact form
+const contactForm = document.getElementById("contact-form");
+const loader = document.querySelector(".loader");
+
+contactForm.addEventListener("submit", function (e) {
+  loader.style.display = "block";
+  e.preventDefault();
+
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
+
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      //  url success
+      loader.style.display = "none";
+      window.location.href = "/success.html";
+    })
+    .catch((e) => alert("Error occured"));
+});
