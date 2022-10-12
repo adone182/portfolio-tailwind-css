ed02721942343081d6b731d4af89b4c74f31a1ef
Romadoni <romadoniofficial@gmail.com>
2022-10-13 00:38:01 +0700

    update profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
-  darkMode:'class',
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
-      center:true,
-      padding:'50px',
+      center: true,
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#14b8a6',
-        secondary:'#64748b',
-        dark: '#0f172a'
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
       screens: {
-      '2xl': '1320px',
-    },
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
